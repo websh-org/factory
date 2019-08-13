8f411643cd5ea9f9faae866e950a0deb6fc80944
Zoran Obradović <zockyo@gmail.com>
2019-08-13 12:33:09 +0200

    ...

--- scripts/includes/build.sh
+++ scripts/includes/build.sh
@@ -14,12 +14,14 @@ echo Bulding package $(tput bold)$PACKAGE$(tput sgr0)
 announce "Is the working directory clean?"
 is_git_clean && ok || abort "Working directory must be clean.";
 
-## prepare the dist directory
-rm -rf $DIST
-mkdir $DIST
-cp $ROOT/README.md $DIST
-cp $ROOT/LICENSE.md $DIST
-cp $ROOT/real.package.json $DIST/package.json
+announce "Preparing the dist directory in $DIST"
+{
+  rm -rf $DIST;
+  mkdir $DIST;
+  cp $ROOT/README.md $DIST;
+  cp $ROOT/LICENSE.md $DIST;
+  cp $ROOT/real.package.json $DIST/package.json;
+} ok || abort
 
 # Build this particular package in the dist directory
 ## Customize as necessary.
